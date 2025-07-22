5bbe8a6df8c5626d8557e659044741b0a9a4f5d0
VitalyL <vetallrssn@yandex.ru>
2025-07-22 11:44:06 +0300

    Добавить spring-webmvc

--- src/main/java/ru/practicum/TestServlet.java
+++ src/main/java/ru/practicum/TestServlet.java
@@ -3,6 +3,7 @@ package ru.practicum;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 public class TestServlet extends HttpServlet {
